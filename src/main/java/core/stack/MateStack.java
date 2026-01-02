4f205e38b0d305c4eecf2aa021d7ede945f4cbff
Przemysław Ciżmiński <cizminski@interia.pl>
2026-01-02 20:37:08 +0100

    Change solution according to AI mentor feddback. Node class is now working in one direction.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -5,12 +5,12 @@ import java.util.EmptyStackException;
 public class MateStack<T> {
     private Node<T> tail = new Node<>(null, null);
     private int arrSize = 0;
+
     public void push(T value) {
         if (arrSize == 0) {
-            tail.item =  value;
-        }
-        else {
-            tail =  new Node<>(tail, value);
+            tail.item = value;
+        } else {
+            tail = new Node<>(tail, value);
         }
         arrSize++;
     }
@@ -23,11 +23,11 @@ public class MateStack<T> {
     }
 
     public T pop() {
-        T value = tail.item;
         if (arrSize == 0) {
             throw new EmptyStackException();
         }
         arrSize--;
+        T value = tail.item;
         tail = tail.prev;
         return value;
     }
