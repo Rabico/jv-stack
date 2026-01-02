a71b0071f9f7b2aa1bbac338f72dc7983ddadcb8
Przemysław Ciżmiński <cizminski@interia.pl>
2026-01-02 19:14:55 +0100

    Created first version to solve task using LinkedList.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,35 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
+import java.util.LinkedList;
 
+public class MateStack<T> {
+    private LinkedList<T> list = new LinkedList<>();
+    private int size = 0;
+
+    public void push(T value) {
+    if (!list.contains(value)) {
+        list.add(value);
+        size++;
+    }
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return list.peekLast();
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        size --;
+        return list.removeLast();
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
